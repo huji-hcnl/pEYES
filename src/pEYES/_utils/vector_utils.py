dccd8b41a3fa32e5d5662ba8218d621dbe9d7817
JonNir <jonathan.nir@mail.huji.ac.il>
2024-06-15 12:04:35 +0300

    add utils

--- src/pEYES/_utils/vector_utils.py
+++ src/pEYES/_utils/vector_utils.py
@@ -1,3 +1,4 @@
+import copy
 from typing import List
 
 import numpy as np
@@ -24,4 +25,47 @@ def get_chunk_indices(arr) -> List[np.ndarray]:
     split_on = np.nonzero(np.diff(arr))[0] + 1  # +1 because we want to include the last index of each chunk
     chunk_indices = np.split(indices, split_on)
     return chunk_indices
+
+
+def merge_chunks(arr: np.ndarray, max_samples_between: int) -> np.ndarray:
+    """
+    Splits the input array into chunks of identical values. If two chunks of the same value are separated by a chunk
+    of a different value, and the middle chunk is shorter/equal to `max_samples_between`, it is set to the same value as
+    the other two chunks.
+    Returns a new array with the merged chunks.
+    """
+    if not is_one_dimensional(arr):
+        raise ValueError("input array must be one-dimensional")
+    if max_samples_between < 0:
+        raise ValueError("max_samples_between must be non-negative")
+    arr_copy = copy.deepcopy(arr)
+    chunk_indices = get_chunk_indices(arr_copy)
+    for i, middle_chunk in enumerate(chunk_indices[1:-1], start=1):
+        if len(middle_chunk) > max_samples_between:
+            # skip if the chunk is too long
+            continue
+        left_chunk_val = arr_copy[chunk_indices[i - 1][0]]
+        right_chunk_val = arr_copy[chunk_indices[i + 1][0]]
+        if left_chunk_val != right_chunk_val:
+            # skip if the left and right chunks have different values
+            continue
+        arr_copy[middle_chunk] = left_chunk_val
+    return arr_copy
+
+
+def reset_short_chunks(arr: np.ndarray, min_samples: int, default_value: int) -> np.ndarray:
+    """
+    Splits the input array into chunks of identical values. If a chunk is shorter than `min_samples`, it is set to the
+    `default_value`. Returns a new array with the reset chunks.
+    """
+    if not is_one_dimensional(arr):
+        raise ValueError("input array must be one-dimensional")
+    if min_samples < 0:
+        raise ValueError("min_samples must be non-negative")
+    arr_copy = copy.deepcopy(arr)
+    chunk_indices = get_chunk_indices(arr_copy)
+    for chunk in chunk_indices:
+        if len(chunk) < min_samples:
+            arr_copy[chunk] = default_value
+    return arr_copy
 
--- tests/unit_tests/utils/test_vector_utils.py
+++ tests/unit_tests/utils/test_vector_utils.py
@@ -27,3 +27,23 @@ class TestVectorUtils(unittest.TestCase):
         obs = get_chunk_indices(arr)
         exp = [np.array([i]) for i in range(10)]
         self.assertTrue(all([np.array_equal(o, e, equal_nan=True) for o, e in zip(obs, exp)]))
+
+    def test_merge_chunks(self):
+        arr = np.array([1, 1, 1, 1, 2, 2, 1, 1, 1])
+        self.assertTrue(np.array_equal(merge_chunks(arr, 2), np.ones_like(arr)))
+        self.assertTrue(np.array_equal(merge_chunks(arr, 1), arr))
+        arr = np.array([1, 1, 1, 1, 2, 2, 3, 3, 3])
+        self.assertTrue(np.array_equal(merge_chunks(arr, 2), arr))
+        self.assertTrue(np.array_equal(merge_chunks(arr, 1), arr))
+        self.assertRaises(ValueError, merge_chunks, np.array([[1, 2], [3, 4]]), 2)
+        self.assertRaises(ValueError, merge_chunks, np.array([1, 2, 3]), -1)
+
+    def test_reset_short_chunks(self):
+        arr = np.array([1, 1, 1, 1, 2, 2, 1, 1, 1])
+        self.assertTrue(np.array_equal(reset_short_chunks(arr, 3, 0), np.array([1, 1, 1, 1, 0, 0, 1, 1, 1])))
+        self.assertTrue(np.array_equal(reset_short_chunks(arr, 2, 0), arr))
+        arr = np.array([1, 1, 1, 1, 2, 2, 3, 3, 3])
+        self.assertTrue(np.array_equal(reset_short_chunks(arr, 3, 0), np.array([1, 1, 1, 1, 0, 0, 3, 3, 3])))
+        self.assertTrue(np.array_equal(reset_short_chunks(arr, 2, 0), arr))
+        self.assertRaises(ValueError, reset_short_chunks, np.array([[1, 2], [3, 4]]), 2, 0)
+        self.assertRaises(ValueError, reset_short_chunks, np.array([1, 2, 3]), -1, 0)
