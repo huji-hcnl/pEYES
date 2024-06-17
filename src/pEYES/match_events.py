43543110b936c0681e14749544201f0d8697a1c6
JonNir <jonathan.nir@mail.huji.ac.il>
2024-06-17 14:01:05 +0300

    naming

--- src/pEYES/match_events.py
+++ src/pEYES/match_events.py
@@ -7,7 +7,7 @@ from src.pEYES._DataModels.EventMatcher import EventMatcher, EventMatchesType
 
 def match_events(
         ground_truth: Sequence[BaseEvent],
-        predictions: Sequence[BaseEvent],
+        prediction: Sequence[BaseEvent],
         match_by: str,
         ignore_events: Set[EventLabelEnum] = None,
         allow_xmatch: bool = False,
@@ -18,7 +18,7 @@ def match_events(
     Matches can be one-to-one or one-to-many depending on the matching criteria and the specified parameters.
 
     :param ground_truth: a sequence of BaseEvent objects representing the ground truth events.
-    :param predictions: a sequence of BaseEvent objects representing the predicted events.
+    :param prediction: a sequence of BaseEvent objects representing the predicted events.
     :param match_by: the matching criteria to use:
         - 'first' or 'first overlap': match the first predicted event that overlaps with each ground-truth event
         - 'last' or 'last overlap': match the last predicted event that overlaps with each ground-truth event
@@ -38,53 +38,53 @@ def match_events(
     match_by = match_by.lower().replace("_", " ").replace("-", " ").strip()
     allow_xmatch = allow_xmatch or match_kwargs.pop("allow_xmatch", None) or match_kwargs.pop("allow_cross_match", False)
     ground_truth = [e for e in ground_truth if e.label not in ignore_events]
-    predictions = [e for e in predictions if e.label not in ignore_events]
+    prediction = [e for e in prediction if e.label not in ignore_events]
     if match_by == "first" or match_by == "first overlap":
         return EventMatcher.first_overlap(
-            ground_truth, predictions, min_overlap=match_kwargs.pop("min_overlap", 0), allow_cross_matching=allow_xmatch
+            ground_truth, prediction, min_overlap=match_kwargs.pop("min_overlap", 0), allow_cross_matching=allow_xmatch
         )
     if match_by == "last" or match_by == "last overlap":
         return EventMatcher.last_overlap(
-            ground_truth, predictions, min_overlap=match_kwargs.pop("min_overlap", 0), allow_cross_matching=allow_xmatch
+            ground_truth, prediction, min_overlap=match_kwargs.pop("min_overlap", 0), allow_cross_matching=allow_xmatch
         )
     if match_by == "max" or match_by == "max overlap":
         return EventMatcher.max_overlap(
-            ground_truth, predictions, min_overlap=match_kwargs.pop("min_overlap", 0), allow_cross_matching=allow_xmatch
+            ground_truth, prediction, min_overlap=match_kwargs.pop("min_overlap", 0), allow_cross_matching=allow_xmatch
         )
     if "longest" in match_by and "overlap" in match_by:
         return EventMatcher.longest_overlapping_event(
-            ground_truth, predictions, min_overlap=match_kwargs.pop("min_overlap", 0), allow_cross_matching=allow_xmatch
+            ground_truth, prediction, min_overlap=match_kwargs.pop("min_overlap", 0), allow_cross_matching=allow_xmatch
         )
     if match_by == "iou" or match_by == "intersection over union":
         return EventMatcher.iou(
-            ground_truth, predictions, min_iou=match_kwargs.pop("min_iou", 0), allow_cross_matching=allow_xmatch
+            ground_truth, prediction, min_iou=match_kwargs.pop("min_iou", 0), allow_cross_matching=allow_xmatch
         )
     if match_by == "onset" or match_by == "onset difference":
         return EventMatcher.onset_difference(
             ground_truth,
-            predictions,
+            prediction,
             max_onset_difference=match_kwargs.pop("max_onset_difference", 0),
             allow_cross_matching=allow_xmatch
         )
     if match_by == "offset" or match_by == "offset max_onset_difference":
         return EventMatcher.offset_difference(
             ground_truth,
-            predictions,
+            prediction,
             max_offset_difference=match_kwargs.pop("max_offset_difference", 0),
             allow_cross_matching=allow_xmatch
         )
     if match_by == "window" or match_by == "window based":
         return EventMatcher.window_based(
             ground_truth,
-            predictions,
+            prediction,
             max_onset_difference=match_kwargs.pop("max_onset_difference", 0),
             max_offset_difference=match_kwargs.pop("max_offset_difference", 0),
             allow_cross_matching=allow_xmatch
         )
     if "l2" in match_by:
         return EventMatcher.l2_timing(
-            ground_truth, predictions, max_l2=match_kwargs.pop("max_l2", 0), allow_cross_matching=allow_xmatch
+            ground_truth, prediction, max_l2=match_kwargs.pop("max_l2", 0), allow_cross_matching=allow_xmatch
         )
     return EventMatcher.generic_matching(
-        ground_truth, predictions, allow_cross_matching=allow_xmatch, **match_kwargs
+        ground_truth, prediction, allow_cross_matching=allow_xmatch, **match_kwargs
     )
