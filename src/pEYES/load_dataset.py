602d513ffa75f51069c107953a17046e3f8bb315
JonNir <jonathan.nir@mail.huji.ac.il>
2024-06-17 12:30:03 +0300

    improve docs

--- src/pEYES/load_dataset.py
+++ src/pEYES/load_dataset.py
@@ -5,16 +5,27 @@ from src.pEYES._DataModels.DatasetLoader import (
 )
 
 
-def load_dataset(dataset_name: str, directory: str = None, should_save: bool = False) -> pd.DataFrame:
+def load_dataset(dataset_name: str, directory: str = None, save: bool = False) -> pd.DataFrame:
+    """
+    Loads the dataset from the specified directory. If the directory is not specified or the dataset is not found, it is
+    downloaded from the internet. If `save` is True and the dataset was downloaded, it is saved to the specified
+    directory.
+
+    :param dataset_name: str; the name of the dataset to load, one of: "Lund2013", "IRF", "HFC", "GazeCom"
+    :param directory: str; the directory to load the dataset from or save the dataset to
+    :param save: bool; if True, the dataset is saved to the specified directory
+    :return: a DataFrame containing the dataset
+    :raises ValueError: if `save` is True and `directory` is not specified
+    """
     dataset_name_lower = dataset_name.lower().strip()
-    if should_save and not directory:
+    if save and not directory:
         raise ValueError("Directory must be specified to save the dataset")
     if dataset_name_lower == "lund2013":
-        return Lund2013DatasetLoader.load(directory, should_save)
+        return Lund2013DatasetLoader.load(directory, save)
     if dataset_name_lower == "irf":
-        return IRFDatasetLoader.load(directory, should_save)
+        return IRFDatasetLoader.load(directory, save)
     if dataset_name_lower == "hfc":
-        return HFCDatasetLoader.load(directory, should_save)
+        return HFCDatasetLoader.load(directory, save)
     if dataset_name_lower == "gazecom":
-        return GazeComDatasetLoader.load(directory, should_save)
+        return GazeComDatasetLoader.load(directory, save)
     raise NotImplementedError(f"Unknown dataset: {dataset_name}")
