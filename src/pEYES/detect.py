02523adbc66fbe8052d46a6b3e7455712c211731
JonNir <jonathan.nir@mail.huji.ac.il>
2024-06-17 13:13:27 +0300

    make common detector args mandatory, and detector-specific args as keywords. update docs

--- src/pEYES/detect.py
+++ src/pEYES/detect.py
@@ -2,22 +2,87 @@ from typing import Union, Tuple, Dict, Any
 
 import numpy as np
 
+import src.pEYES.config as cnfg
 from src.pEYES._DataModels.Detector import (
     IVTDetector, IVVTDetector, IDTDetector, EngbertDetector, NHDetector, REMoDNaVDetector
 )
 
 
 def detect(
-        detector_name: str,
         t: np.ndarray,
         x: np.ndarray,
         y: np.ndarray,
         viewer_distance: float,
         pixel_size: float,
+        detector_name: str,
+        missing_value: float = np.nan,
+        min_event_duration: float = cnfg.MIN_EVENT_DURATION,
+        pad_blinks_time: float = 0,
         include_metadata: bool = False,
         **kwargs
 ) -> Union[np.ndarray, Tuple[np.ndarray, Dict[str, Any]]]:
-    detector = _create_detector(detector_name, **kwargs)
+    """
+    Detects gaze events in the given gaze data using the specified detector, and returns the detected labels for each
+    sample. Optionally, returns the metadata of the detection process.
+
+    :param t: np.ndarray; the timestamps of the gaze data, in milliseconds
+    :param x: np.ndarray; the x-coordinates of the gaze data, in pixels
+    :param y: np.ndarray; the y-coordinates of the gaze data, in pixels
+    :param viewer_distance: float; the distance between the viewer and the screen, in centimeters
+    :param pixel_size: float; the size of a pixel on the screen, in centimeters
+    :param detector_name: str; the name of the detector to use.
+        Must be one of: "IVT", "IVVT", "IDT", "Engbert", "NH", "REMoDNaV"
+    :param missing_value: float; the value used to represent missing data in the gaze data
+    :param min_event_duration: float; the minimum duration of a gaze event, in milliseconds
+    :param pad_blinks_time: float; the duration to pad before and after each detected blink event, in milliseconds
+    :param include_metadata: bool; if True, also returns the metadata of the detection process
+
+    Additional keyword arguments required for detector initialization:
+    - IVT:
+        :keyword saccade_velocity_threshold: float; velocity threshold to separates saccades from fixations (deg/s), default: 45
+    - IVVT:
+        :keyword saccade_velocity_threshold: float; velocity threshold to separates saccades from smooth pursuits (deg/s), default: 45
+        :keyword smooth_pursuit_velocity_threshold: float; the velocity threshold that separates smooth pursuits from fixations (deg/s), default: 5
+    - IDT:
+        :keyword dispersion_threshold: dispersion threshold that separates saccades from fixations (visual angle degrees), default: 0.5
+        :keyword window_duration: the duration of the window used to calculate the dispersion threshold (ms), default: 100
+    - Engbert:
+        :keyword lambda_param: float; multiplication coefficient used for calculating saccade threshold, default: 5
+        :keyword deriv_window_size: int; number of samples used to compute axial velocity, default: 5
+    - NH:
+        :keyword filter_duration_ms: int; Savitzky-Golay filter's duration (ms), default: 20
+        :keyword filter_polyorder: int; Savitzky-Golay filter's polynomial order, default: 2
+        :keyword saccade_max_velocity: maximum saccade velocity (deg/s), default: 1000
+        :keyword saccade_max_acceleration: maximum saccade acceleration (deg/s^2), default: 100000
+        :keyword min_saccade_duration: minimum saccade duration (ms), default: 10
+        :keyword min_fixation_duration: minimum fixation duration (ms), default: 50
+        :keyword max_pso_duration: maximum PSO duration (ms), default: 80
+        :keyword alpha_param: weight of saccade onset threshold when detecting saccade offset, default: 0.7
+        :keyword ignore_short_peak_durations: if True, excludes sporadic occurrences of peak velocity when detecting
+            saccade peaks, default: True
+        :keyword allow_high_psos: if True, includes "high" PSOs, i.e., PSOs with max velocity exceeding saccades' peak threshold but lower than the preceding saccade, default: True
+    - REMoDNaV:
+        :keyword min_saccade_duration: int; the minimum duration of a saccade (ms), default: 10
+        :keyword saccade_initial_velocity_threshold: float; the initial velocity threshold for saccade detection (deg/s), default: 300
+        :keyword saccade_context_window_duration: int; the duration of the context window for saccade detection (ms), default: 1000
+        :keyword saccade_initial_max_freq: float; the initial maximum frequency for saccade detection (Hz), default: 2.0
+        :keyword saccade_onset_threshold_noise_factor: float; the noise factor for saccade onset threshold, default: 5.0
+        :keyword min_smooth_pursuit_duration: int; the minimum duration of a smooth pursuit (ms), default: 40
+        :keyword smooth_pursuits_lowpass_cutoff_freq: float; the lowpass cutoff frequency for smooth pursuit detection (Hz), default: 4.0
+        :keyword smooth_pursuit_drift_velocity_threshold: float; the drift velocity threshold for smooth pursuit detection (deg/s), default: 2.0
+        :keyword min_fixation_duration: int; the minimum duration of a fixation (ms), default: 50
+        :keyword min_blink_duration: int; the minimum duration of a blink (ms), default: 20
+        :keyword max_pso_duration: int; the maximum duration of a PSO (ms), default: 80
+        :keyword savgol_filter_polyorder: int; the polynomial order for the Savitzky-Golay filter, default: 2
+        :keyword savgol_filter_duration_ms: int; the duration of the Savitzky-Golay filter (ms), default: 19
+        :keyword median_filter_duration_ms: int; the duration of the median filter (ms), default: 50
+        :keyword max_velocity: float; the maximum velocity of the gaze data (deg/s), default: 1500
+
+    :return:
+        labels: np.ndarray; the detected labels for each sample
+        metadata: Dict[str, Any]; the metadata of the detection process, returned only if `include_metadata` is True
+    """
+    detector = _create_detector(detector_name, missing_value, min_event_duration, pad_blinks_time, **kwargs)
     labels, metadata = detector.detect(t, x, y, viewer_distance, pixel_size)
     if include_metadata:
         return labels, metadata
@@ -26,32 +91,47 @@ def detect(
 
 def _create_detector(
         detector_name: str,
+        missing_value: float,
+        min_event_duration: float,
+        pad_blinks_time: float,
         **kwargs
 ):
-    detector_name_lower = detector_name.lower().strip().replace('-', '')
+    detector_name_lower = detector_name.lower().strip().replace('-', '').removesuffix('detector')
     if detector_name_lower == 'ivt':
         default_params = IVTDetector.get_default_params()
         kwargs = {**default_params, **kwargs}
-        return IVTDetector(**kwargs)
+        return IVTDetector(
+            missing_value=missing_value, min_event_duration=min_event_duration, pad_blinks_ms=pad_blinks_time, **kwargs
+        )
     elif detector_name_lower == 'ivvt':
         default_params = IVVTDetector.get_default_params()
         kwargs = {**default_params, **kwargs}
-        return IVVTDetector(**kwargs)
+        return IVVTDetector(
+            missing_value=missing_value, min_event_duration=min_event_duration, pad_blinks_ms=pad_blinks_time, **kwargs
+        )
     elif detector_name_lower == 'idt':
         default_params = IDTDetector.get_default_params()
         kwargs = {**default_params, **kwargs}
-        return IDTDetector(**kwargs)
+        return IDTDetector(
+            missing_value=missing_value, min_event_duration=min_event_duration, pad_blinks_ms=pad_blinks_time, **kwargs
+        )
     elif detector_name_lower == 'engbert':
         default_params = EngbertDetector.get_default_params()
         kwargs = {**default_params, **kwargs}
-        return EngbertDetector(**kwargs)
+        return EngbertDetector(
+            missing_value=missing_value, min_event_duration=min_event_duration, pad_blinks_ms=pad_blinks_time, **kwargs
+        )
     elif detector_name_lower == 'nh':
         default_params = NHDetector.get_default_params()
         kwargs = {**default_params, **kwargs}
-        return NHDetector(**kwargs)
+        return NHDetector(
+            missing_value=missing_value, min_event_duration=min_event_duration, pad_blinks_ms=pad_blinks_time, **kwargs
+        )
     elif detector_name_lower == 'remodnav':
         default_params = REMoDNaVDetector.get_default_params()
         kwargs = {**default_params, **kwargs}
-        return REMoDNaVDetector(**kwargs)
+        return REMoDNaVDetector(
+            missing_value=missing_value, min_event_duration=min_event_duration, pad_blinks_ms=pad_blinks_time, **kwargs
+        )
     else:
         raise NotImplementedError(f'Detector `{detector_name}` is not implemented.')
