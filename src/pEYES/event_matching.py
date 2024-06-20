2e035ff98430f78924f0088a281672ef1e81e33c
JonNir <jonathan.nir@mail.huji.ac.il>
2024-06-20 14:27:39 +0300

    improve naming and add docstring

--- src/pEYES/event_matching.py
+++ src/pEYES/event_matching.py
@@ -119,12 +119,30 @@ def match_multiple(
 
 
 def calculate(
-        matches: OneToOneEventMatchesType, metrics: Union[str, Sequence[str]], verbose: bool = False,
+        matches: OneToOneEventMatchesType, features: Union[str, Sequence[str]], verbose: bool = False,
 ) -> Union[np.ndarray, Dict[str, np.ndarray]]:
-    if isinstance(metrics, str):
-        return _calculate_impl(matches, metrics)
+    """
+    Calculates the difference in the specified features between matched ground-truth and predicted events.
+    :param matches: the one-to-one matches between ground-truth and predicted events
+    :param features: the feature(s) to calculate the difference for. Supported features are:
+        - 'onset' or 'onset_difference': difference in onset time (ms)
+        - 'offset' or 'offset_difference': difference in offset time (ms)
+        - 'duration' or 'duration_difference': difference in duration (ms)
+        - 'amplitude' or 'amplitude_difference': difference in amplitude (deg)
+        - 'azimuth' or 'azimuth_difference': difference in azimuth (deg)
+        - 'center_pixel_distance': distance between the center of the ground-truth and predicted events (pixels)
+        - 'time_overlap': total overlap time between the ground-truth and predicted event (ms)
+        - 'time_iou': intersection-over-union between the ground-truth and predicted event (unitless, [0, 1])
+        - 'time_l2': L2 norm of the timing difference between the ground-truth and predicted event (ms)
+    :param verbose: if True, display a progress bar while calculating the metrics
+    :return: the calculated feature differences as a numpy array (for a single feature) or a dictionary of numpy arrays
+        (for multiple features).
+    :raises ValueError: if an unknown feature is provided
+    """
+    if isinstance(features, str):
+        return _calculate_impl(matches, features)
     results = {}
-    for metric in tqdm(metrics, desc="Calculating metrics", disable=not verbose):
+    for metric in tqdm(features, desc="Calculating metrics", disable=not verbose):
         results[metric] = _calculate_impl(matches, metric)
     return results
 
@@ -133,9 +151,19 @@ def precision_recall_f1(
         ground_truth: Sequence[BaseEvent],
         prediction: Sequence[BaseEvent],
         matches: OneToOneEventMatchesType,
-        labels: Union[EventLabelEnum, Sequence[EventLabelEnum]],
+        positive_labels: Union[EventLabelEnum, Sequence[EventLabelEnum]],
 ) -> (float, float, float):
-    p, n, pp, tp = _extract_contingency_values(ground_truth, prediction, matches, labels)
+    """
+    Calculates the precision, recall, and F1-score for the given ground-truth and predicted events, where successfully
+    matched events are considered true positives. The provided labels are considered as "positive" events.
+
+    :param ground_truth: all ground-truth events
+    :param prediction: all predicted events
+    :param matches: the one-to-one matches between (subset of) ground-truth and (subset of) predicted events
+    :param positive_labels: event-labels to consider as "positive" events
+    :return: the precision, recall, and F1-score values
+    """
+    p, n, pp, tp = _extract_contingency_values(ground_truth, prediction, matches, positive_labels)
     recall = tp / p if p > 0 else np.nan
     precision = tp / pp if pp > 0 else np.nan
     f1 = 2 * (precision * recall) / (precision + recall) if precision + recall > 0 else np.nan
@@ -146,7 +174,7 @@ def d_prime(
         ground_truth: Sequence[BaseEvent],
         prediction: Sequence[BaseEvent],
         matches: OneToOneEventMatchesType,
-        labels: Union[EventLabelEnum, Sequence[EventLabelEnum]],
+        positive_labels: Union[EventLabelEnum, Sequence[EventLabelEnum]],
         correction: Optional[str] = "loglinear",
 ) -> float:
     """
@@ -156,43 +184,43 @@ def d_prime(
     :param ground_truth: all ground-truth events
     :param prediction: all predicted events
     :param matches: the one-to-one matches between (subset of) ground-truth and (subset of) predicted events
-    :param labels: event-labels to consider as "positive" events
+    :param positive_labels: event-labels to consider as "positive" events
     :param correction: optional floor/ceiling correction method when calculating hit-rate and false-alarm rate
     :return: the d-prime value
     """
-    p, n, pp, tp = _extract_contingency_values(ground_truth, prediction, matches, labels)
+    p, n, pp, tp = _extract_contingency_values(ground_truth, prediction, matches, positive_labels)
     return dprime(p, n, pp, tp, correction)
 
 
-def _calculate_impl(matches: OneToOneEventMatchesType, metric: str,) -> np.ndarray:
-    metric_name = metric.lower().strip().replace(" ", "_").replace("-", "_")
-    metric_name = metric_name.removesuffix("_difference")
-    if metric_name == "onset":
+def _calculate_impl(matches: OneToOneEventMatchesType, feature: str,) -> np.ndarray:
+    feature_name = feature.lower().strip().replace(" ", "_").replace("-", "_")
+    feature_name = feature_name.removesuffix("_difference")
+    if feature_name == "onset":
         return np.array([gt.start_time - pred.start_time for gt, pred in matches.items()])
-    if metric_name == "offset":
+    if feature_name == "offset":
         return np.array([gt.end_time - pred.end_time for gt, pred in matches.items()])
-    if metric_name == "duration":
+    if feature_name == "duration":
         return np.array([gt.duration - pred.duration for gt, pred in matches.items()])
-    if metric_name == "amplitude":
+    if feature_name == "amplitude":
         return np.array([gt.amplitude - pred.amplitude for gt, pred in matches.items()])
-    if metric_name == "azimuth":
+    if feature_name == "azimuth":
         return np.array([gt.azimuth - pred.azimuth for gt, pred in matches.items()])
-    if metric_name == "center_pixel_distance":
+    if feature_name == "center_pixel_distance":
         return np.array([gt.center_distance(pred) for gt, pred in matches.items()])
-    if metric_name == "time_overlap":
+    if feature_name == "time_overlap":
         return np.array([gt.time_overlap(pred) for gt, pred in matches.items()])
-    if metric_name == "time_iou":
+    if feature_name == "time_iou":
         return np.array([gt.time_iou(pred) for gt, pred in matches.items()])
-    if metric_name == "time_l2":
+    if feature_name == "time_l2":
         return np.array([gt.time_l2(pred) for gt, pred in matches.items()])
-    raise ValueError(f"Unknown metric: {metric}")
+    raise ValueError(f"Unknown feature: {feature}")
 
 
 def _extract_contingency_values(
         ground_truth: Sequence[BaseEvent],
         prediction: Sequence[BaseEvent],
         matches: OneToOneEventMatchesType,
-        labels: Union[EventLabelEnum, Sequence[EventLabelEnum]],
+        positive_labels: Union[EventLabelEnum, Sequence[EventLabelEnum]],
 ) -> (int, int, int, int):
     """
     Extracts contingency values, used to fill in the confusion matrix for the provided matches between ground-truth and
@@ -204,9 +232,9 @@ def _extract_contingency_values(
         pp: int; number of positive predicted events
         tp: int; number of true positive predictions
     """
-    labels = [labels] if isinstance(labels, EventLabelEnum) else labels
-    p = len([e for e in ground_truth if e.label in labels])
+    positive_labels = [positive_labels] if isinstance(positive_labels, EventLabelEnum) else positive_labels
+    p = len([e for e in ground_truth if e.label in positive_labels])
     n = len(ground_truth) - p
-    pp = len([e for e in prediction if e.label in labels])
-    tp = len([e for e in matches.values() if e.label in labels])
+    pp = len([e for e in prediction if e.label in positive_labels])
+    tp = len([e for e in matches.values() if e.label in positive_labels])
     return p, n, pp, tp
