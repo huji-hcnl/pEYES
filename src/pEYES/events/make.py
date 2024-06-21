f48bb7b05f78b46dbdfdb4d191069b21dbbf50cc
JonNir <jonathan.nir@mail.huji.ac.il>
2024-06-21 11:02:02 +0300

    add docstring

--- src/pEYES/events/make.py
+++ src/pEYES/events/make.py
@@ -16,15 +16,19 @@ def make(
         pixel_size: float,
 ) -> Union[Event, List[Event]]:
     """
-    TODO: Add documentation here
-    :param labels:
-    :param t:
-    :param x:
-    :param y:
-    :param pupil:
-    :param viewer_distance:
-    :param pixel_size:
-    :return:
+    Create gaze-events from the given data.
+    If `labels` is a single label, creates a single event spanning the entire data.
+    Otherwise, `labels` must have the same length as `t` and events will be created for each "chunk" of subsequent
+    identical labels.
+
+    :param labels: The event label(s)
+    :param t: timestamps (ms)
+    :param x: horizontal gaze position (pixels)
+    :param y: vertical gaze position (pixels)
+    :param pupil: pupil size (mm)
+    :param viewer_distance: distance from the viewer's eyes to the screen (cm)
+    :param pixel_size: size of a pixel on the screen (cm)
+    :return: the generated event(s)
     """
     if isinstance(labels, UnparsedEventLabelType):
         label = parse_label(labels)
