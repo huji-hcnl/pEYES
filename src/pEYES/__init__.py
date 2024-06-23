1bf780d25d561a791be5745c8b2675491b4a93b6
JonNir <jonathan.nir@mail.huji.ac.il>
2024-06-23 13:28:13 +0300

    bugfixes

--- src/pEYES/__init__.py
+++ src/pEYES/__init__.py
@@ -1,5 +1,8 @@
 
 __version__ = '0.1.0'  # TODO: update automatically
 
+import src.pEYES.constants as constants
+import src.pEYES.config as config
+
 from src.pEYES.load_dataset import load_dataset
 from src.pEYES.detect import detect, detect_multiple
