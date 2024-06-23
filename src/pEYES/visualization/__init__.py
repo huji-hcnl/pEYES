99e665d71e8fbb7c1257bdf02ec7fac6da1e2207
JonNir <jonathan.nir@mail.huji.ac.il>
2024-06-23 12:35:50 +0300

    add main sequence figures

--- src/pEYES/visualization/__init__.py
+++ src/pEYES/visualization/__init__.py
@@ -1,2 +1,5 @@
 
 from src.pEYES.visualization.scarfplot import scarfplot_comparison_figure, add_scarfplot
+from src.pEYES.visualization.main_sequence import peak_velocity_to_amplitude as peak_velocity_main_sequence
+from src.pEYES.visualization.main_sequence import duration_to_amplitude as duration_main_sequence
+from src.pEYES.visualization.video import create_video, create_frames
