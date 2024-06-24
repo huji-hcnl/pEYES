2d07d8e5909ed65f4981d11a1ccf3a28f27fb77f
JonNir <jonathan.nir@mail.huji.ac.il>
2024-06-24 17:29:28 +0300

    add `datasets` sub package

--- src/pEYES/process/__init__.py
+++ src/pEYES/process/__init__.py
@@ -1,5 +1,4 @@
 
-from src.pEYES.process.load_dataset import load_dataset
 from src.pEYES.process.detect import detect, detect_multiple
 from src.pEYES.process.events import create_events, events_to_labels, events_to_table
 from src.pEYES.process.match import match, match_multiple
