4c28aa4a756bf583499cf4874e95439d8badbc1c
JonNir <jonathan.nir@mail.huji.ac.il>
2024-06-26 15:18:05 +0300

    naming

--- src/pEYES/process/__init__.py
+++ src/pEYES/process/__init__.py
@@ -1,6 +1,5 @@
 
 from src.pEYES.process.parse import parse
 from src.pEYES.process.create import detector, events
-from src.pEYES.process.detect import detect, detect_multiple
-from src.pEYES.process.events import create_events, events_to_labels, summarize_events
+from src.pEYES.process.postprocess_events import events_to_labels, summarize_events
 from src.pEYES.process.match import match, match_multiple
