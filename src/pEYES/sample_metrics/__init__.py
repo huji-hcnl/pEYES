30fd324e1f1ee7535429f6551a44d16791e13506
JonNir <jonathan.nir@mail.huji.ac.il>
2024-06-27 13:33:11 +0300

    improve typing

--- src/pEYES/sample_metrics/__init__.py
+++ src/pEYES/sample_metrics/__init__.py
@@ -1,57 +1,55 @@
-from typing import Sequence, Dict, Union, Optional
+from typing import Union, Optional
 
-import numpy as np
-
-from src.pEYES._DataModels.EventLabelEnum import EventLabelEnum as EventLabel
-
+from src.pEYES._DataModels.EventLabelEnum import EventLabelSequenceType
+from src.pEYES._DataModels.UnparsedEventLabel import UnparsedEventLabelType, UnparsedEventLabelSequenceType
 
 from src.pEYES.sample_metrics.confusion_matrix import confusion_matrix
 from src.pEYES.sample_metrics.transition_matrix import transition_matrix
 from src.pEYES.sample_metrics.calculate_metrics import calculate
 
 
-def accuracy(ground_truth: Sequence[EventLabel], prediction: Sequence[EventLabel],) -> float:
+def accuracy(ground_truth: EventLabelSequenceType, prediction: EventLabelSequenceType,) -> float:
     return calculate(ground_truth, prediction, "accuracy")
 
 
-def balanced_accuracy(ground_truth: Sequence[EventLabel], prediction: Sequence[EventLabel],) -> float:
+def balanced_accuracy(ground_truth: EventLabelSequenceType, prediction: EventLabelSequenceType,) -> float:
     return calculate(ground_truth, prediction, "balanced_accuracy")
 
 
-def cohen_kappa(ground_truth: Sequence[EventLabel], prediction: Sequence[EventLabel],) -> float:
+def cohen_kappa(ground_truth: EventLabelSequenceType, prediction: EventLabelSequenceType,) -> float:
     return calculate(ground_truth, prediction, "cohen's_kappa")
 
 
-def mcc(ground_truth: Sequence[EventLabel], prediction: Sequence[EventLabel],) -> float:
+def mcc(ground_truth: EventLabelSequenceType, prediction: EventLabelSequenceType,) -> float:
     return calculate(ground_truth, prediction, "mcc")
 
 
-def complement_nld(ground_truth: Sequence[EventLabel], prediction: Sequence[EventLabel],) -> float:
+def complement_nld(ground_truth: EventLabelSequenceType, prediction: EventLabelSequenceType,) -> float:
     return calculate(ground_truth, prediction, "1_nld")
 
 
 def precision(
-        ground_truth: Sequence[EventLabel],
-        prediction: Sequence[EventLabel],
-        pos_labels: Optional[Union[EventLabel, Sequence[EventLabel]]] = None,
+        ground_truth: EventLabelSequenceType,
+        prediction: EventLabelSequenceType,
+        pos_labels: Optional[Union[UnparsedEventLabelType, UnparsedEventLabelSequenceType]] = None,
         average: str = "weighted",
 ) -> float:
     return calculate(ground_truth, prediction, "precision", pos_labels=pos_labels, average=average)
 
 
 def recall(
-        ground_truth: Sequence[EventLabel],
-        prediction: Sequence[EventLabel],
-        pos_labels: Optional[Union[EventLabel, Sequence[EventLabel]]] = None,
+        ground_truth: EventLabelSequenceType,
+        prediction: EventLabelSequenceType,
+        pos_labels: Optional[Union[UnparsedEventLabelType, UnparsedEventLabelSequenceType]] = None,
         average: str = "weighted",
 ) -> float:
     return calculate(ground_truth, prediction, "recall", pos_labels=pos_labels, average=average)
 
 
 def f1_score(
-        ground_truth: Sequence[EventLabel],
-        prediction: Sequence[EventLabel],
-        pos_labels: Optional[Union[EventLabel, Sequence[EventLabel]]] = None,
+        ground_truth: EventLabelSequenceType,
+        prediction: EventLabelSequenceType,
+        pos_labels: Optional[Union[UnparsedEventLabelType, UnparsedEventLabelSequenceType]] = None,
         average: str = "weighted",
 ) -> float:
     return calculate(ground_truth, prediction, "f1", pos_labels=pos_labels, average=average)
