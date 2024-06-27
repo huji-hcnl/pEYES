558282f0bd352cb42cba5f65463dbfc7d9fa3415
JonNir <jonathan.nir@mail.huji.ac.il>
2024-06-27 13:50:06 +0300

    fix doc

--- src/pEYES/_base_scripts/parse.py
+++ src/pEYES/_base_scripts/parse.py
@@ -12,21 +12,17 @@ def parse(
         x_name: str = cnst.X,
         y_name: str = cnst.Y,
         pupil_name: str = cnst.PUPIL,
-        viewer_distance_name: str = cnst.VIEWER_DISTANCE_STR,
-        pixel_size_name: str = cnst.PIXEL_SIZE_STR,
         missing_data_value: float = np.nan,
 ):
     """
-    Parse raw gaze data into a DataFrame, and rename the columns to the standard names: `t`, `x`, `y`, `pupil`,
-    `viewer_distance`, and `pixel_size`. Samples of missing data are replaced with NaN.
+    Parse raw gaze data into a DataFrame, and rename the columns to the standard names: `t`, `x`, `y`, `pupil`.
+    Missing columns are filled with NaN, and missing data is replaced with NaN.
 
     :param data: DataFrame or named-array containing the raw gaze data
     :param time_name: name of the time column (ms)
     :param x_name: name of the horizontal gaze position column (pixels)
     :param y_name: name of the vertical gaze position column (pixels)
     :param pupil_name: name of the pupil size column (mm)
-    :param viewer_distance_name: name of the viewer distance column (cm)
-    :param pixel_size_name: name of the pixel size column (cm)
     :param missing_data_value: the value used to represent missing data in the raw data
 
     :return: a DataFrame containing the parsed gaze data with renamed to columns to the standard names
@@ -39,13 +35,11 @@ def parse(
         raise TypeError(f"Invalid dataset type: {type(data)}")
 
     # rename columns
-    data.rename(
-        columns={
-            time_name: cnst.T, x_name: cnst.X, y_name: cnst.Y, pupil_name: cnst.PUPIL,
-            viewer_distance_name: cnst.VIEWER_DISTANCE_STR, pixel_size_name: cnst.PIXEL_SIZE_STR
-        },
-        inplace=True
-    )
+    columns_rename = {time_name: cnst.T, x_name: cnst.X, y_name: cnst.Y, pupil_name: cnst.PUPIL}
+    data.rename(columns=columns_rename, inplace=True)
+    missing_columns = [col for col in columns_rename.values() if col not in data.columns]
+    for col in missing_columns:
+        data.loc[:, col] = np.nan
 
     # replace missing data with NaN
     if np.isnan(missing_data_value):
