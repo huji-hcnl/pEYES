8b8c5870ba438c365b97d3f7e24ae2fc81031c52
JonNir <jonathan.nir@mail.huji.ac.il>
2024-06-27 16:27:44 +0300

    naming

--- src/pEYES/event_metrics/__init__.py
+++ src/pEYES/event_metrics/__init__.py
@@ -1,39 +1,40 @@
-from src.pEYES._DataModels.Event import EventSequenceType
-from src.pEYES._DataModels.EventLabelEnum import EventLabelEnum
+from src.pEYES._DataModels.Event import EventSequenceType as _EventSequenceType
+from src.pEYES._DataModels.EventLabelEnum import EventLabelEnum as _EventLabelEnum
+
+from src.pEYES.event_metrics.get_features import get_features as _features
+from src.pEYES.event_metrics.counts_and_rates import event_rate as _event_rate
 
 from src.pEYES.event_metrics.counts_and_rates import counts, microsaccade_ratio
 from src.pEYES.event_metrics.transition_matrix import transition_matrix
-from src.pEYES.event_metrics.get_features import get_features as features
-from src.pEYES.event_metrics.counts_and_rates import event_rate as _event_rate
 
 
-def start_times(events: EventSequenceType):
-    return features(events, "start_time", verbose=False)
+def start_times(events: _EventSequenceType):
+    return _features(events, "start_time", verbose=False)
 
 
-def end_times(events: EventSequenceType):
-    return features(events, "end_time", verbose=False)
+def end_times(events: _EventSequenceType):
+    return _features(events, "end_time", verbose=False)
 
 
-def durations(events: EventSequenceType):
-    return features(events, "duration", verbose=False)
+def durations(events: _EventSequenceType):
+    return _features(events, "duration", verbose=False)
 
 
-def amplitudes(events: EventSequenceType):
-    return features(events, "amplitude", verbose=False)
+def amplitudes(events: _EventSequenceType):
+    return _features(events, "amplitude", verbose=False)
 
 
-def azimuths(events: EventSequenceType):
-    return features(events, "azimuth", verbose=False)
+def azimuths(events: _EventSequenceType):
+    return _features(events, "azimuth", verbose=False)
 
 
-def center_pixels(events: EventSequenceType):
-    return features(events, "center_pixel", verbose=False)
+def center_pixels(events: _EventSequenceType):
+    return _features(events, "center_pixel", verbose=False)
 
 
-def saccade_rate(events: EventSequenceType):
-    return _event_rate(events, EventLabelEnum.SACCADE)
+def saccade_rate(events: _EventSequenceType):
+    return _event_rate(events, _EventLabelEnum.SACCADE)
 
 
-def blink_rate(events: EventSequenceType):
-    return _event_rate(events, EventLabelEnum.BLINK)
+def blink_rate(events: _EventSequenceType):
+    return _event_rate(events, _EventLabelEnum.BLINK)
