6d8b0dc21f8b18e582df0a55d22d2a4084667a44
JonNir <jonathan.nir@mail.huji.ac.il>
2024-06-27 18:52:58 +0300

    add visualization todos

---
+++ src/pEYES/visualization/zz_event_summary.py
@@ -0,0 +1,17 @@
+
+# TODO: all-event summary figure - a multi-panel figure with the following panels:
+    # event counts (bar plot)
+    # event durations (histogram / box plot / violin plot)
+    # event amplitudes (histogram / box plot / violin plot)
+    # event amplitude vs duration (scatter plot with colors based on event labels)
+
+# TODO: saccade summary figure - a multi-panel figure with the following panels:
+    # saccade durations histogram / violin
+    # saccade amplitudes histogram / violin
+    # saccade azimuth histogram (radial histogram)
+    # saccade main sequence (peak velocity vs. amplitude)
+
+# TODO: fixation summary figure - a multi-panel figure with the following panels:
+    # fixation durations histogram / violin
+    # fixation center locations (heatmap or just scatter plot)
+    # fixation dispersion (pixel std) histogram / violin
---
+++ src/pEYES/visualization/zz_gaze_visualization.py
@@ -0,0 +1,8 @@
+
+
+# TODO: create figure of x-y coordinates over time with option to add velocity and acceleration vectors, and option to
+# add event labels as scarfplot / vertical lines
+
+# TODO: create figure of y-coordinates over x-coordinates with color changing through time
+
+# TODO: create figure of y-coordinates over x-coordinates with color changing according to event labels
