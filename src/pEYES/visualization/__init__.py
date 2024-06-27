7e4dfcf327e87941941117a8e4fdc343091afcc4
JonNir <jonathan.nir@mail.huji.ac.il>
2024-06-27 18:42:37 +0300

    delete unready

--- src/pEYES/visualization/__init__.py
+++ src/pEYES/visualization/__init__.py
@@ -2,7 +2,3 @@
 from src.pEYES.visualization.scarfplot import scarfplot_comparison_figure, add_scarfplot
 from src.pEYES.visualization.heatmap import heatmap
 from src.pEYES.visualization.video import create_video, create_frames
-
-# TODO: FIX THESE:
-from src.pEYES.visualization.main_sequence import peak_velocity_to_amplitude as peak_velocity_main_sequence
-from src.pEYES.visualization.main_sequence import duration_to_amplitude as duration_main_sequence
