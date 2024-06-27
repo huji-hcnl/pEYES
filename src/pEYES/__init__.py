61078de1ed1c2723d7c681558b0b718391c29190
JonNir <jonathan.nir@mail.huji.ac.il>
2024-06-27 15:51:27 +0300

    nit

--- src/pEYES/__init__.py
+++ src/pEYES/__init__.py
@@ -1,12 +1,12 @@
 
 __version__ = '0.1.0'  # TODO: update automatically
 
+import src.pEYES._base.config as config
 from src.pEYES._base.parse import parse
 from src.pEYES._base.create import create_detector, create_events
 from src.pEYES._base.match import match
 from src.pEYES._base.postprocess_events import events_to_labels, summarize_events
 
-import src.pEYES._base.config as config
 import src.pEYES.datasets as datasets
 import src.pEYES.event_metrics as event_metrics
 import src.pEYES.sample_metrics as sample_metrics
