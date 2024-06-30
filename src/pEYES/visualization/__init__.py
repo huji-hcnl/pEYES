d3cfdf914d7d34ba641e9d85748e6e8786ac9028
JonNir <jonathan.nir@mail.huji.ac.il>
2024-06-30 14:41:49 +0300

    gaze visualizations

--- src/pEYES/visualization/__init__.py
+++ src/pEYES/visualization/__init__.py
@@ -1,4 +1,4 @@
 
+from src.pEYES.visualization.gaze import gaze_trajectory, gaze_heatmap, gaze_over_time
 from src.pEYES.visualization.scarfplot import scarfplot_comparison_figure, add_scarfplot
-from src.pEYES.visualization.heatmap import heatmap
 from src.pEYES.visualization.video import create_video, create_frames
--- src/pEYES/visualization/zz_gaze_visualization.py
+++
@@ -1,8 +0,0 @@
-
-
-# TODO: create figure of x-y coordinates over time with option to add velocity and acceleration vectors, and option to
-# add event labels as scarfplot / vertical lines
-
-# TODO: create figure of y-coordinates over x-coordinates with color changing through time
-
-# TODO: create figure of y-coordinates over x-coordinates with color changing according to event labels
