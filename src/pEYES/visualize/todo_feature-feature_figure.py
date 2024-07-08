9e7bb7fe7c2886a081612fd1e303392d91c63c69
JonNir <jonathan.nir@mail.huji.ac.il>
2024-07-08 14:43:31 +0300

    example from plotly express

--- src/pEYES/visualize/todo_feature-feature_figure.py
+++ src/pEYES/visualize/todo_feature-feature_figure.py
@@ -1,11 +1,21 @@
 import numpy as np
 import plotly.graph_objects as go
+import plotly.express as px
 
 from src.pEYES._DataModels.Event import EventSequenceType
 from src.pEYES._DataModels.EventLabelEnum import EventLabelEnum
 
 
 # TODO: FIX THESE FIGURES + ADD DOCSTRINGS + CALCULATE RANSAC REGRESSION LINES
+
+fig1 = px.scatter(
+    summary[summary[cnst.LABEL_STR] == 2], x=cnst.AMPLITUDE_STR, y=cnst.DURATION_STR, color="is_outlier",
+    log_x=False, log_y=False, marginal_x='violin', marginal_y="violin", trendline="ols",
+    trendline_options=dict(log_x=False, log_y=False)
+)
+fig1.show()
+
+px.get_trendline_results(fig1).iloc[0, 1].summary()
 
 
 def peak_velocity_to_amplitude(events: EventSequenceType) -> go.Figure:
