f627c757e31e7cbac96f5c5a87909e3c0dd29579
JonNir <jonathan.nir@mail.huji.ac.il>
2024-07-08 10:34:02 +0300

    rename

--- src/pEYES/visualize/__init__.py
+++ src/pEYES/visualize/__init__.py
@@ -1,4 +1,4 @@
 
-from src.pEYES.visualize.gaze import gaze_trajectory, gaze_heatmap, gaze_over_time
-from src.pEYES.visualize.scarfplot import scarfplot_comparison_figure, add_scarfplot_to_figure
-from src.pEYES.visualize.video import create_video, create_frames
+from src.pEYES.visualize._gaze import gaze_trajectory, gaze_heatmap, gaze_over_time
+from src.pEYES.visualize._scarfplot import scarfplot_comparison_figure, add_scarfplot_to_figure
+from src.pEYES.visualize._video import create_video, create_frames
