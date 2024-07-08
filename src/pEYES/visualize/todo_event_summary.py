4b003601b62ec3ecb8c5cd52e144fc55cc6e2d4d
JonNir <jonathan.nir@mail.huji.ac.il>
2024-07-08 13:34:44 +0300

    add docstring & kwargs

--- src/pEYES/visualize/todo_event_summary.py
+++ src/pEYES/visualize/todo_event_summary.py
@@ -14,16 +14,30 @@ __INLIERS_STR, __OUTLIERS_STR = "inliers", "outliers"
 
 def event_summary(
         events: EventSequenceType,
-        title: str = "Event Summary",
-        label_colors: vis_utils.LabelColormapType = None,
-        outlier_opacity: float = 1/3,
-        show_outliers: bool = True,
+        **kwargs,
 ) -> go.Figure:
+    """
+    Creates a summary figure of the provided events. The figure contains four panels:
+        1. A bar plot of the count of each event label.
+        2. A violin plot showing the distribution of durations per event label.
+        3. A violin plot showing the distribution of amplitudes per event label.
+        4. A violin plot showing the distribution of peak velocities per event label.
+
+    :param events: list (or other sequence) of events.
+    :keyword label_colors: A dictionary mapping event labels to their respective colors. If a label is missing, the
+        default color is used.
+    :keyword show_outliers: whether to show outliers in the violin plots; default is False.
+    :keyword outlier_opacity: if `show_outliers` is true, this controls the opacity of the outliers; default is 0.5.
+    :keyword title: the title of the figure; default is "Event Summary".
+
+    :return: the figure.
+    """
     inlier_features = features_by_labels(list(filter(lambda e: not e.is_outlier, events)))
     outlier_features = features_by_labels(list(filter(lambda e: e.is_outlier, events)))
     assert inlier_features.index.equals(outlier_features.index)
     event_labels = [parse_label(l) for l in inlier_features.index]
-    label_colors = vis_utils.get_label_colormap(label_colors)
+    label_colors = vis_utils.get_label_colormap(kwargs.get("label_colors", None))
+    show_outliers = kwargs.get("show_outliers", False)
 
     subplots = {
         cnst.COUNT_STR: '# instances',
@@ -54,7 +68,7 @@ def event_summary(
                         trace=go.Bar(
                             x0=evnt.name,
                             y=[outlier_features.loc[evnt, metric]],
-                            marker_color=color, opacity=outlier_opacity,
+                            marker_color=color, opacity=kwargs.get("outlier_opacity", 0.5),
                             name=f"{evnt.name} ({__OUTLIERS_STR})", legendgroup=evnt.name, showlegend=False,
                         )
                     )
@@ -76,15 +90,15 @@ def event_summary(
                         trace=go.Violin(
                             x0=evnt.name,
                             y=outlier_features.loc[evnt, metric],
-                            fillcolor=color, spanmode='hard', showlegend=False, opacity=outlier_opacity,
-                            side='negative',
+                            fillcolor=color, spanmode='hard', showlegend=False, side='negative',
+                            opacity=kwargs.get("outlier_opacity", 0.5),
                             box_visible=True, meanline_visible=True, line_color='black',
                             name=f"{evnt.name} ({__OUTLIERS_STR})", legendgroup=evnt.name,
                         )
                     )
         fig.update_yaxes(col=1, row=r+1, title_text=measure)
     fig.update_layout(
-        title=title,
+        title=kwargs.get("title", f"{cnst.EVENT_STR.title()} Summary"),
         barmode='stack',
         violinmode='overlay',
         violingap=0,
