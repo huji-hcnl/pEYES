2e3a99165ad60cdff39df478aedcf1701621df96
JonNir <jonathan.nir@mail.huji.ac.il>
2024-07-12 12:21:49 +0300

    nit - simplify code (make it a bit more verbose)

--- src/pEYES/visualize/todo_event_summary.py
+++ src/pEYES/visualize/todo_event_summary.py
@@ -14,10 +14,7 @@ from src.pEYES._DataModels.EventLabelEnum import EventLabelEnum
 __INLIERS_STR, __OUTLIERS_STR = "inliers", "outliers"
 
 
-def event_summary(
-        events: EventSequenceType,
-        **kwargs,
-) -> go.Figure:
+def event_summary(events: EventSequenceType, **kwargs) -> go.Figure:
     """
     Creates a summary figure of the provided events. The figure contains four panels:
         1. A bar plot of the count of each event label.
@@ -40,7 +37,6 @@ def event_summary(
     event_labels = [parse_label(l) for l in inlier_features.index]
     label_colors = vis_utils.get_label_colormap(kwargs.get("label_colors", None))
     show_outliers = kwargs.get("show_outliers", False)
-
     subplots = {
         cnst.COUNT_STR: '# instances',
         cnst.DURATION_STR: 'ms',
@@ -52,53 +48,45 @@ def event_summary(
         subplot_titles=list(map(lambda met: met.replace('_', ' ').title(), subplots.keys()))
     )
     for r, (metric, measure) in enumerate(subplots.items()):
+        fig.update_yaxes(col=1, row=r + 1, title_text=measure)
         for evnt in event_labels:
+            outlier_trace = None
             color = f"rgb{label_colors[evnt]}"
             if metric == cnst.COUNT_STR:
-                fig.add_trace(
-                    col=1, row=r+1,
-                    trace=go.Bar(
-                        x0=evnt.name,
-                        y=[inlier_features.loc[evnt, metric]],
-                        marker_color=color,
-                        name=evnt.name, legendgroup=evnt.name, showlegend=True,
-                    )
+                inlier_trace = go.Bar(
+                    x0=evnt.name,
+                    y=[inlier_features.loc[evnt, metric]],
+                    marker_color=color,
+                    name=evnt.name, legendgroup=evnt.name, showlegend=True,
                 )
                 if show_outliers:
-                    fig.add_trace(
-                        col=1, row=r+1,
-                        trace=go.Bar(
-                            x0=evnt.name,
-                            y=[outlier_features.loc[evnt, metric]],
-                            marker_color=color, opacity=kwargs.get("outlier_opacity", 0.5),
-                            name=f"{evnt.name} ({__OUTLIERS_STR})", legendgroup=evnt.name, showlegend=False,
-                        )
+                    outlier_trace = go.Bar(
+                        x0=evnt.name,
+                        y=[outlier_features.loc[evnt, metric]],
+                        marker_color=color, opacity=kwargs.get("outlier_opacity", 0.5),
+                        name=f"{evnt.name} ({__OUTLIERS_STR})", legendgroup=evnt.name, showlegend=False,
                     )
             else:
-                fig.add_trace(
-                    col=1, row=r+1,
-                    trace=go.Violin(
-                        x0=evnt.name,
-                        y=inlier_features.loc[evnt, metric],
-                        fillcolor=color, spanmode='hard', showlegend=False,
-                        side='positive' if show_outliers else None,
-                        box_visible=True, meanline_visible=True, line_color='black',
-                        name=evnt.name, legendgroup=evnt.name,
-                    )
+                inlier_trace = go.Violin(
+                    x0=evnt.name,
+                    y=inlier_features.loc[evnt, metric],
+                    fillcolor=color, spanmode='hard', showlegend=False,
+                    side='positive' if show_outliers else None,
+                    box_visible=True, meanline_visible=True, line_color='black',
+                    name=evnt.name, legendgroup=evnt.name,
                 )
                 if show_outliers:
-                    fig.add_trace(
-                        col=1, row=r+1,
-                        trace=go.Violin(
-                            x0=evnt.name,
-                            y=outlier_features.loc[evnt, metric],
-                            fillcolor=color, spanmode='hard', showlegend=False, side='negative',
-                            opacity=kwargs.get("outlier_opacity", 0.5),
-                            box_visible=True, meanline_visible=True, line_color='black',
-                            name=f"{evnt.name} ({__OUTLIERS_STR})", legendgroup=evnt.name,
-                        )
+                    outlier_trace = go.Violin(
+                        x0=evnt.name,
+                        y=outlier_features.loc[evnt, metric],
+                        fillcolor=color, spanmode='hard', showlegend=False, side='negative',
+                        opacity=kwargs.get("outlier_opacity", 0.5),
+                        box_visible=True, meanline_visible=True, line_color='black',
+                        name=f"{evnt.name} ({__OUTLIERS_STR})", legendgroup=evnt.name,
                     )
-        fig.update_yaxes(col=1, row=r+1, title_text=measure)
+            fig.add_trace(inlier_trace, col=1, row=r+1)
+            if show_outliers and outlier_trace is not None:
+                fig.add_trace(outlier_trace, col=1, row=r+1)
     fig.update_layout(
         title=kwargs.get("title", f"{cnst.EVENT_STR.title()} Summary"),
         barmode='stack',
@@ -109,21 +97,15 @@ def event_summary(
     return fig
 
 
-def fixation_summary(
-        fixations: EventSequenceType,
-        **kwargs,
-) -> go.Figure:
+def fixation_summary(fixations: EventSequenceType, **kwargs) -> go.Figure:
     summary_df = summarize_events(list(filter(lambda e: e.label == EventLabelEnum.FIXATION, fixations)))
     inlier_df, outlier_df = summary_df[~summary_df[cnst.IS_OUTLIER_STR]], summary_df[summary_df[cnst.IS_OUTLIER_STR]]
-    inlier_color = kwargs.get("inlier_color", f"rgb{vis_utils.get_label_colormap(None)[EventLabelEnum.FIXATION]}")
     show_outliers = kwargs.get("show_outliers", False)
-    outlier_color = kwargs.get("outlier_color", f"rgb{vis_utils.get_label_colormap(None)[EventLabelEnum.FIXATION]}")
-    opacity = kwargs.get("opacity", 0.5)
     subplots = {
         cnst.DURATION_STR: 'ms',
-        cnst.CENTER_PIXEL_STR: 'x coordinate',
         cnst.PIXEL_STD_STR: 'px',
         cnst.PEAK_VELOCITY_STR: 'deg/s',
+        cnst.CENTER_PIXEL_STR: 'x coordinate',
     }
     fig = make_subplots(
         rows=2, cols=2,
@@ -131,36 +113,54 @@ def fixation_summary(
     )
     for i, (metric, measure) in enumerate(subplots.items()):
         r, c = divmod(i, 2)
+        fig.update_xaxes(title_text=measure, row=r + 1, col=c + 1)
+        outlier_trace = None
         if metric == cnst.CENTER_PIXEL_STR:
+            fig.update_yaxes(title_text="y coordinate", row=r + 1, col=c + 1)
             px = pd.DataFrame(inlier_df[cnst.CENTER_PIXEL_STR].to_list(), columns=[cnst.X, cnst.Y])
             inlier_trace = go.Scatter(
                 x=px[cnst.X], y=px[cnst.Y], mode="markers", name=None, showlegend=False,
-                marker=dict(size=kwargs.get("marker_size", 5), color=inlier_color),
+                marker=dict(
+                    size=kwargs.get("marker_size", 5),
+                    color=kwargs.get(
+                        "inlier_color", f"rgb{vis_utils.get_label_colormap(None)[EventLabelEnum.FIXATION]}"
+                    )
+                ),
             )
-            fig.update_yaxes(title_text="y coordinate", row=r + 1, col=c + 1)
             if show_outliers:
                 px = pd.DataFrame(outlier_df[cnst.CENTER_PIXEL_STR].to_list(), columns=[cnst.X, cnst.Y])
                 outlier_trace = go.Scatter(
                     x=px[cnst.X], y=px[cnst.Y], mode="markers", name=None, showlegend=False,
-                    marker=dict(size=kwargs.get("marker_size", 5), color=outlier_color, opacity=opacity),
+                    marker=dict(
+                        size=kwargs.get("marker_size", 5),
+                        color=kwargs.get(
+                            "outlier_color", f"rgb{vis_utils.get_label_colormap(None)[EventLabelEnum.FIXATION]}"
+                        ),
+                        opacity=kwargs.get("outlier_opacity", 0.5)
+                    ),
                 )
         else:
             inlier_trace = go.Violin(
                 x=inlier_df[metric], spanmode='hard', side='positive', showlegend=False,
                 name=metric, legendgroup=metric, scalegroup=metric,
-                fillcolor=inlier_color, box_visible=True, meanline_visible=True, line_color='black',
+                fillcolor=kwargs.get(
+                    "inlier_color", f"rgb{vis_utils.get_label_colormap(None)[EventLabelEnum.FIXATION]}"
+                ),
+                box_visible=True, meanline_visible=True, line_color='black',
             )
             if show_outliers:
                 outlier_trace = go.Violin(
                     x=outlier_df[metric], spanmode='hard', side='positive', showlegend=False,
                     name=metric, legendgroup=metric, scalegroup=metric,
-                    fillcolor=outlier_color, opacity=opacity,
+                    opacity=kwargs.get("outlier_opacity", 0.5),
+                    fillcolor=kwargs.get(
+                        "outlier_color", f"rgb{vis_utils.get_label_colormap(None)[EventLabelEnum.FIXATION]}"
+                    ),
                     box_visible=True, meanline_visible=True, line_color='black',
                 )
         fig.add_trace(inlier_trace, row=r+1, col=c+1)
-        if show_outliers:
+        if show_outliers and outlier_trace is not None:
             fig.add_trace(outlier_trace, row=r+1, col=c+1)
-        fig.update_xaxes(title_text=measure, row=r+1, col=c+1)
     fig.update_layout(
         title=kwargs.get("title", f"{EventLabelEnum.FIXATION.name.title()} Summary"),
         violinmode='overlay',
