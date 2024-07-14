d823a7cf298ac6553db1b6dd7aa04afd3b79a7e8
JonNir <jonathan.nir@mail.huji.ac.il>
2024-07-14 16:16:13 +0300

    nit

--- src/pEYES/_base/postprocess_events.py
+++ src/pEYES/_base/postprocess_events.py
@@ -1,20 +1,15 @@
-from typing import Union, Sequence
-
 import numpy as np
 import pandas as pd
 
 from src.pEYES._utils import constants as cnst
-from src.pEYES._DataModels.Event import BaseEvent, EventSequenceType
-from src.pEYES._DataModels.EventLabelEnum import EventLabelEnum
+from src.pEYES._DataModels.Event import EventSequenceType
+from src.pEYES._DataModels.EventLabelEnum import EventLabelEnum, EventLabelSequenceType
 
 
 def summarize_events(
-        events: Union[BaseEvent, EventSequenceType],
+        events: EventSequenceType,
 ) -> pd.DataFrame:
-    """ Converts the given event(s) to a DataFrame, where each row is an event and columns are event features. """
-    if isinstance(events, BaseEvent):
-        s = events.summary()
-        return s.to_frame()
+    """ Converts the given events to a DataFrame, where each row is an event and columns are event features. """
     if len(events) == 0:
         return pd.DataFrame()
     summaries = [e.summary() for e in events]
@@ -22,25 +17,21 @@ def summarize_events(
 
 
 def events_to_labels(
-        events: Union[BaseEvent, EventSequenceType],
+        events: EventSequenceType,
         sampling_rate: float,
         num_samples=None,
-) -> Sequence[EventLabelEnum]:
+) -> EventLabelSequenceType:
     """
-    Converts the given event(s) to a sequence of labels, where each event is mapped to a sequence of labels with length
+    Converts the given events to a sequence of labels, where each event is mapped to a sequence of labels with length
     matching the number of samples in the event's duration (rounded up to the nearest integer).
     Samples with no event are labeled as `EventLabelEnum.UNDEFINED`.
 
-    :param events: sequence of event(s)
+    :param events: sequence of events
     :param sampling_rate: the sampling rate of the output labels
     :param num_samples: the number of samples in the output sequence. If None, the number of samples is determined by
         the total duration of the provided events.
     :return: sequence of labels
     """
-    if isinstance(events, BaseEvent):
-        out = np.full(int(np.ceil(sampling_rate * events.duration / cnst.MILLISECONDS_PER_SECOND)), events.label)
-        return out
-
     max_end_time = max(e.end_time for e in events)
     min_num_samples = int(np.ceil(sampling_rate * max_end_time / cnst.MILLISECONDS_PER_SECOND))
     if num_samples is not None and num_samples < min_num_samples:
