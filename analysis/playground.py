a36ee88234f3666e2bfa3d3b190ab7cb81da825c
JonNir <jonathan.nir@mail.huji.ac.il>
2024-07-15 15:59:32 +0300

    nit

--- analysis/playground.py
+++ analysis/playground.py
@@ -23,3 +23,18 @@ pio.renderers.default = "browser"
 dataset, labels, events, metadata = u.default_load_or_process("lund2013", verbose=False)
 
 ###################
+
+for tr in labels.index.get_level_values(level=peyes.TRIAL_ID_STR):
+    for gt_labeler in u.DATASET_ANNOTATORS['lund2013']:
+        gt_labels = labels.loc[tr, gt_labeler].dropna().values.flatten()
+        for other_labeler in labels.columns.get_level_values(u.LABELER_STR).unique():
+            if other_labeler == gt_labeler or other_labeler == "MN":
+                continue
+            other_labels_df = labels.loc[tr, other_labeler]
+            for it in other_labels_df.columns.get_level_values(peyes.ITERATION_STR).unique():
+                other_labels = other_labels_df[it].dropna().values.flatten()
+                res = peyes.sample_metrics.calculate(gt_labels, other_labels)
+                break
+            break
+        break
+    break
