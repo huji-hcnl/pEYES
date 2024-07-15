aece6322a6e620e76305701913dddf539272f731
JonNir <jonathan.nir@mail.huji.ac.il>
2024-07-15 14:09:02 +0300

    update analysis

--- analysis/playground.py
+++ analysis/playground.py
@@ -18,103 +18,38 @@ import analysis.utils as u
 
 pio.renderers.default = "browser"
 
-#########
+###################
 
-dataset_name = "lund2013"
-num_iterations = 4
-verbose = True
+dataset = u.load_dataset("lund2013", verbose=True)
+labels, events, metadata = u.process_dataset(
+    dataset,
+    detectors=u.DEFAULT_DETECTORS,
+    annotators=u.DATASET_ANNOTATORS["lund2013"],
+    num_iterations=4,
+    overwrite_label=EventLabelEnum.SACCADE,
+    verbose=False
+)
 
+###################
 
-def _load_dataset(dataset_name: str, verbose: bool = True) -> pd.DataFrame:
-    if dataset_name == "lund2013":
-        dataset = peyes.datasets.lund2013(directory=u.DATASETS_DIR, save=True, verbose=verbose)
-    elif dataset_name == "irf":
-        dataset = peyes.datasets.irf(directory=u.DATASETS_DIR, save=True, verbose=verbose)
-    elif dataset_name == "hfc":
-        dataset = peyes.datasets.hfc(directory=u.DATASETS_DIR, save=True, verbose=verbose)
-    else:
-        raise ValueError(f"Unknown dataset name: {dataset_name}")
-    return dataset
+# dataset = peyes.datasets.lund2013(directory=u.DATASETS_DIR, save=False, verbose=True)
+# multi_trial_events = []
+# for i in range(1, 21):
+#     trial = dataset[dataset[peyes.TRIAL_ID_STR] == i]
+#     ra_events = peyes.create_events(
+#         labels=trial['RA'].values,
+#         t=trial[peyes.T].values,
+#         x=trial[peyes.X].values,
+#         y=trial[peyes.Y].values,
+#         pupil=trial[peyes.PUPIL].values,
+#         viewer_distance=trial[peyes.VIEWER_DISTANCE_STR].iloc[0],
+#         pixel_size=trial[peyes.PIXEL_SIZE_STR].iloc[0]
+#     )
+#     multi_trial_events.extend(ra_events)
+# summary = peyes.summarize_events(multi_trial_events)
+# features = peyes.event_metrics.features_by_labels(multi_trial_events)
+# labels = [parse_label(l) for l in features.index]
+#
+# del i, trial, ra_events
 
-
-def process_dataset(
-        dataset_name: str, num_iterations: int = 4, verbose: bool = True
-):
-    dataset = _load_dataset(dataset_name, verbose=verbose)
-    detectors = [peyes.create_detector(det, missing_value=np.nan, min_event_duration=4, pad_blinks_time=0)
-                 for det in u.DETECTOR_NAMES]
-    labels, events, metadata = dict(), dict(), dict()
-    for tr in tqdm(dataset[peyes.TRIAL_ID_STR].unique(), desc="Trials", leave=True, disable=not verbose):
-        if tr == 2:
-            break
-        tr_labels, tr_events, tr_metadata = dict(), dict(), dict()
-        trial = dataset[dataset[peyes.TRIAL_ID_STR] == tr]
-        t = trial[peyes.T].values
-        x = trial[peyes.X].values
-        y = trial[peyes.Y].values
-        p = trial[peyes.PUPIL].values
-        vd = trial[peyes.VIEWER_DISTANCE_STR].iloc[0]
-        ps = trial[peyes.PIXEL_SIZE_STR].iloc[0]
-
-        for annot in tqdm(u.DATASET_ANNOTATORS[dataset_name], desc="\tAnnotators", leave=False, disable=not verbose):
-            tr_labels[(annot, 1)] = pd.Series(trial[annot].values, name=(annot))
-            tr_events[(annot, 1)] = peyes.create_events(
-                labels=trial[annot].values, t=t, x=x, y=y, pupil=p, viewer_distance=vd, pixel_size=ps
-            )
-
-        for det in tqdm(detectors, desc="\tDetectors", leave=False, disable=not verbose):
-            det_labels, det_events, det_metadata = dict(), dict(), dict()
-            det_name = det.name
-            x_copy, y_copy, p_copy = copy.deepcopy(x), copy.deepcopy(y), copy.deepcopy(p)
-            for it in trange(num_iterations, desc="\t\tIterations", leave=False, disable=not verbose):
-                it_labels, it_metadata = det.detect(t, x_copy, y_copy, viewer_distance_cm=vd, pixel_size_cm=ps)
-                it_events = peyes.create_events(
-                    labels=it_labels, t=t, x=x_copy, y=y_copy, pupil=p_copy, viewer_distance=vd, pixel_size=ps
-                )
-                det_labels[it+1] = it_labels
-                det_events[it+1] = it_events
-                det_metadata[it+1] = it_metadata
-                x_copy[it_labels == EventLabelEnum.SACCADE] = np.nan
-                y_copy[it_labels == EventLabelEnum.SACCADE] = np.nan
-                p_copy[it_labels == EventLabelEnum.SACCADE] = np.nan
-            det_labels = pd.DataFrame(det_labels)
-            det_events = pd.DataFrame(det_events)
-            det_metadata = pd.DataFrame(det_metadata)
-            tr_labels[det_name] = det_labels
-            tr_events[det_name] = det_events
-            tr_metadata[det_name] = det_metadata
-
-        tr_labels = pd.concat(tr_labels, axis=1).T
-        tr_events = pd.concat(tr_events, axis=1).T
-        tr_metadata = pd.concat(tr_metadata, axis=1).T
-
-        labels[tr] = tr_labels
-        events[tr] = tr_events
-        metadata[tr] = tr_metadata
-
-
-    return None
-
-
-
-dataset = peyes.datasets.lund2013(directory=u.DATASETS_DIR, save=False, verbose=True)
-multi_trial_events = []
-for i in range(1, 21):
-    trial = dataset[dataset[peyes.TRIAL_ID_STR] == i]
-    ra_events = peyes.create_events(
-        labels=trial['RA'].values,
-        t=trial[peyes.T].values,
-        x=trial[peyes.X].values,
-        y=trial[peyes.Y].values,
-        pupil=trial[peyes.PUPIL].values,
-        viewer_distance=trial[peyes.VIEWER_DISTANCE_STR].iloc[0],
-        pixel_size=trial[peyes.PIXEL_SIZE_STR].iloc[0]
-    )
-    multi_trial_events.extend(ra_events)
-summary = peyes.summarize_events(multi_trial_events)
-features = peyes.event_metrics.features_by_labels(multi_trial_events)
-labels = [parse_label(l) for l in features.index]
-
-del i, trial, ra_events
-
-#########
+###################
