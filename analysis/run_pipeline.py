28cc1eebf4578625214b7a5530ec58d29db73775
JonNir <jonathan.nir@mail.huji.ac.il>
2024-07-23 17:15:11 +0300

    run pipeline for fixations and saccades

--- analysis/run_pipeline.py
+++ analysis/run_pipeline.py
@@ -10,4 +10,9 @@ pio.renderers.default = "browser"
 ###################
 
 results = run(output_dir=os.path.join(u.OUTPUT_DIR, "default_values"), dataset_name="lund2013", verbose=False)
-
+fix_results = run(
+    output_dir=os.path.join(u.OUTPUT_DIR, "default_values"), dataset_name="lund2013", verbose=False, pos_labels=1
+)
+sac_results = run(
+    output_dir=os.path.join(u.OUTPUT_DIR, "default_values"), dataset_name="lund2013", verbose=False, pos_labels=2
+)
