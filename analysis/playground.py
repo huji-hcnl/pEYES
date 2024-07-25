5406fdc31c212af491789d439f3a2000e08aace7
JonNir <jonathan.nir@mail.huji.ac.il>
2024-07-25 13:52:59 +0300

    uodate

--- analysis/playground.py
+++ analysis/playground.py
@@ -13,10 +13,23 @@ from analysis.pipeline.full_pipeline import run
 
 pio.renderers.default = "browser"
 
-###################
+################
+## ALL LABELS ##
 
 results = run(output_dir=os.path.join(u.OUTPUT_DIR, "default_values"), dataset_name="lund2013", verbose=False)
-(dataset, labels, metadata, events, matches, sample_mets, time_diffs, channel_sdt_metrics, matched_features, matches_sdt_metrics) = results
-del results
+# (dataset, labels, metadata, events, matches, sample_mets, time_diffs, channel_sdt_metrics, matched_features, matches_sdt_metrics) = results
+# del results
 
-###################
+################
+## FIX LABELS ##
+
+results = run(output_dir=os.path.join(u.OUTPUT_DIR, "default_values"), dataset_name="lund2013", verbose=False, pos_labels=1)
+# (dataset, labels, metadata, events, matches, sample_mets, time_diffs, channel_sdt_metrics, matched_features, matches_sdt_metrics) = results
+# del results
+
+################
+## SAC LABELS ##
+
+results = run(output_dir=os.path.join(u.OUTPUT_DIR, "default_values"), dataset_name="lund2013", verbose=False, pos_labels=2)
+# (dataset, labels, metadata, events, matches, sample_mets, time_diffs, channel_sdt_metrics, matched_features, matches_sdt_metrics) = results
+# del results
