ab71378ff26996980db4bbb680af64eaafabd301
JonNir <jonathan.nir@mail.huji.ac.il>
2024-07-27 11:57:17 +0300

    nit

--- analysis/statistics/sample_metrics.py
+++ analysis/statistics/sample_metrics.py
@@ -26,20 +26,26 @@ def get_sample_metrics(
         stimulus_type: Optional[Union[str, List[str]]] = None,
         metric: Optional[Union[str, List[str]]] = None,
 ) -> pd.DataFrame:
-    return h.get_data(dataset_name=dataset_name, output_dir=output_dir,
-                      data_dir_name=f"{peyes.constants.SAMPLE_STR}_{peyes.constants.METRICS_STR}", label=label,
-                      filename_prefix="", filename_suffix="", iteration=1, stimulus_type=stimulus_type,
-                      sub_index=metric)
+    return h.get_data(
+        dataset_name=dataset_name, output_dir=output_dir,
+        data_dir_name=f"{peyes.constants.SAMPLE_STR}_{peyes.constants.METRICS_STR}", label=label,
+        filename_prefix="", filename_suffix="", iteration=1, stimulus_type=stimulus_type,
+        sub_index=metric
+    )
 
 
 ###################
-## EXAMPLE USAGE ##
 
+DATASET_NAME = "lund2013"
 GT1, GT2 = "RA", "MN"
+MULTI_COMP = "fdr_bh"
+
+####################
+## Sample Metrics ##
 
 sample_metrics = get_sample_metrics(
     "lund2013", os.path.join(u.OUTPUT_DIR, "default_values"), label=None, stimulus_type="image", metric=None
 )
-sm_statistics, sm_pvalues, sm_dunns, sm_Ns = h.statistical_analysis(sample_metrics, [GT1, GT2], multi_comp="fdr_bh")
+sm_statistics, sm_pvalues, sm_dunns, sm_Ns = h.statistical_analysis(sample_metrics, [GT1, GT2], multi_comp=MULTI_COMP)
 sample_metrics_fig = h.distributions_figure(sample_metrics, GT1, gt2=GT2, title=f"Sample Metrics", only_box=False)
 sample_metrics_fig.show()
