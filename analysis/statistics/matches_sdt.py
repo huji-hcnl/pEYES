d09a84da93738144769e17cda27b165774360268
JonNir <jonathan.nir@mail.huji.ac.il>
2024-07-29 12:24:16 +0300

    TODO

---
+++ analysis/statistics/matches_sdt.py
@@ -0,0 +1,8 @@
+
+
+###################
+##  Matched SDT  ##
+# TODO:
+# matched_sdt_metrics = pd.read_pickle(os.path.join(u.OUTPUT_DIR, "default_values", "lund2013", "matches_metrics", "fixation_sdt_metrics.pkl"))
+# iter1_matched_sdt_metrics = matched_sdt_metrics.xs(1, level=peyes.constants.ITERATION_STR, axis=1)
+# image_iter1_matched_sdt_metrics = iter1_matched_sdt_metrics.loc[:, iter1_matched_sdt_metrics.columns.get_level_values(peyes.constants.TRIAL_ID_STR).isin(image_trials)]
