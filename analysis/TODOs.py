36ccd2314b2830aed36c91dac4edd9897f67a15f
JonNir <jonathan.nir@mail.huji.ac.il>
2024-08-08 18:46:32 +0300

    add TODOs.py

---
+++ analysis/TODOs.py
@@ -0,0 +1,22 @@
+# TODO: This file is a placeholder for the TODOs that need to be addressed in the analysis/added to peyes
+
+######################
+# Documentation:
+# add example usage in the `docs` directory
+# add documentation for keyword arguments + licensing of each dataset + detector
+
+######################
+# New Detectors:
+# I-DVT - detect fixations by small dispersion, and saccade/SP by velocity
+# ML Detectors: hold an instance of model that can be loaded from file or trained
+#    I-RF - Random Forest detector (with/without XGBoost)
+#    I-1D-CNN - 1D Convolutional Neural Network detector
+
+######################
+# Dataset Loaders:
+# Add support for EEGEyeNet dataset (require premature downloading and enable only loading from disk)?
+
+######################
+# Analysis:
+# Permutation Tests between "similar" detectors?
+# Bayesian analysis?
