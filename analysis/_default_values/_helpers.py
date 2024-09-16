d2f9490debc71b29eb5190c0cc6dde52158ea38d
JonNir <jonathan.nir@mail.huji.ac.il>
2024-09-16 12:03:51 +0300

    nit

--- analysis/_default_values/_helpers.py
+++ analysis/_default_values/_helpers.py
@@ -2,7 +2,11 @@ import os
 
 import analysis.utils as u
 
-PROCESSED_DATA_DIR = os.path.join(u.OUTPUT_DIR, "default_values")
 DATASET_NAME = "lund2013"
-
 ALPHA = 0.05
+
+_DEFAULT_VALUES_STR = "default_values"
+_FIGURES_STR = "figures"
+PROCESSED_DATA_DIR = os.path.join(u.OUTPUT_DIR, _DEFAULT_VALUES_STR)
+FIGURES_DIR = os.path.join(u.OUTPUT_DIR, _DEFAULT_VALUES_STR, _FIGURES_STR)
+os.makedirs(FIGURES_DIR, exist_ok=True)
