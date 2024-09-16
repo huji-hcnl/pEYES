b660814295f8d9d54edad45fb0f088c3e3041a78
JonNir <jonathan.nir@mail.huji.ac.il>
2024-09-16 13:49:53 +0300

    add scarfplot

--- analysis/_default_values/article_figs/fig1_gaze_vis.py
+++ analysis/_default_values/article_figs/fig1_gaze_vis.py
@@ -1,13 +1,14 @@
 import os
 
 import numpy as np
+import pandas as pd
 import cv2
 from plotly.subplots import make_subplots
 import plotly.io as pio
 
 import peyes
 import analysis.utils as u
-from analysis._default_values._helpers import DATASET_NAME, FIGURES_DIR
+from analysis._default_values._helpers import DATASET_NAME, PROCESSED_DATA_DIR, FIGURES_DIR
 
 pio.renderers.default = "browser"
 
@@ -17,6 +18,7 @@ STIMULUS_TYPE = peyes.constants.IMAGE_STR
 STIMULUS_DIR = os.path.join(u.BASE_DIR, "stimuli", DATASET_NAME.capitalize(), STIMULUS_TYPE)
 
 ######################
+# Load Raw Data
 
 dataset = u.load_dataset(DATASET_NAME, verbose=False)
 img_trial_id = u.get_trials_for_stimulus_type(DATASET_NAME, STIMULUS_TYPE)[5]
@@ -27,50 +29,76 @@ img_name += ".png"
 img = cv2.imread(os.path.join(STIMULUS_DIR, img_name))
 resolution = (img.shape[1], img.shape[0])
 
+pixel_size = trial_data[peyes.constants.PIXEL_SIZE_STR].values[0]
+viewer_distance = trial_data[peyes.constants.VIEWER_DISTANCE_STR].values[0]
 t = trial_data[peyes.constants.T].values
 x = trial_data[peyes.constants.X].values
 y = trial_data[peyes.constants.Y].values
-pixel_size = trial_data[peyes.constants.PIXEL_SIZE_STR].values[0]
-viewer_distance = trial_data[peyes.constants.VIEWER_DISTANCE_STR].values[0]
 
 x[(x < 0) | (x >= resolution[0])] = np.nan
 y[(y < 0) | (y >= resolution[1])] = np.nan
 
 ######################
+# Load Labels
 
-t_fig = peyes.visualize.gaze_trajectory(
+labels_df = pd.read_pickle(os.path.join(PROCESSED_DATA_DIR, DATASET_NAME, peyes.constants.LABELS_STR + ".pkl"))
+labels_df = labels_df.xs(1, level=peyes.constants.ITERATION_STR, axis=1)  # Only use first iteration
+labels_df = labels_df.xs(img_trial_id, level=peyes.constants.TRIAL_ID_STR, axis=1)    # Only use the relevant trial
+labels_df = labels_df.dropna(how="all", axis=0)   # Drop rows with all NaNs
+
+labeler_names = sorted(
+    labels_df.columns.get_level_values(peyes.constants.LABELER_STR).unique(),
+    key=lambda d: u.LABELERS_CONFIG[d.strip().lower().removesuffix("detector")][1]
+)
+
+######################
+
+top_fig = peyes.visualize.gaze_trajectory(
     x=x, y=y, resolution=resolution, title="Gaze Trajectory",
     bg_image=img, bg_image_format='rgb',
     t=t, colorscale='Jet'
 )
-t_fig.update_layout(
+top_fig.update_layout(
     title=None,
 )
-t_fig.show()
+top_fig.show()
 
 
-b_fig = peyes.visualize.gaze_over_time(
+middle_fig = peyes.visualize.gaze_over_time(
     x=x, y=y, t=t, resolution=resolution, title="Gaze Over Time",
     v=peyes._utils.pixel_utils.calculate_velocities(x, y, t), v_measure='px/s'
 )
-b_fig.update_layout(
+middle_fig.update_layout(
     paper_bgcolor='rgba(0, 0, 0, 0)',
     plot_bgcolor='rgba(0, 0, 0, 0)',
-    height=t_fig.layout.height // 2,
-    width=t_fig.layout.width,
+    height=top_fig.layout.height // 2,
+    width=top_fig.layout.width,
     title=None,
 )
-b_fig.show()
+middle_fig.show()
+
+bottom_fig = peyes.visualize.scarfplot_comparison_figure(
+    t,
+    *[labels_df[labeler_name] for labeler_name in labeler_names],
+    names=labeler_names,
+)
+bottom_fig.update_layout(
+    height=top_fig.layout.height // 2,
+    width=top_fig.layout.width,
+    title=None,
+)
+bottom_fig.show()
 
 ######################
 
-peyes.visualize.save_figure(t_fig, "1A_gaze_trajectory", FIGURES_DIR, as_png=True, as_html=False, as_json=False)
-peyes.visualize.save_figure(b_fig, "1B_gaze_over_time", FIGURES_DIR, as_png=True, as_html=False, as_json=False)
+peyes.visualize.save_figure(top_fig, "1A_gaze_trajectory", FIGURES_DIR, as_png=True, as_html=False, as_json=False)
+peyes.visualize.save_figure(middle_fig, "1B_gaze_over_time", FIGURES_DIR, as_png=True, as_html=False, as_json=False)
+peyes.visualize.save_figure(bottom_fig, "1C_scarfplot", FIGURES_DIR, as_png=True, as_html=False, as_json=False)
 
 ######################
 
 # combined_fig = make_subplots(cols=1, rows=2, shared_xaxes=False, shared_yaxes=False)
 #
-# [combined_fig.add_trace(fig_data, row=1, col=1) for fig_data in t_fig.data]
-# [combined_fig.add_trace(fig_data, row=2, col=1) for fig_data in b_fig.data]
+# [combined_fig.add_trace(fig_data, row=1, col=1) for fig_data in top_fig.data]
+# [combined_fig.add_trace(fig_data, row=2, col=1) for fig_data in middle_fig.data]
 # combined_fig.show()
