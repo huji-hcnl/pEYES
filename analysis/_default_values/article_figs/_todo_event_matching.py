e11c9e1b735e4f858fc8df6a55a1cba9584998bd
JonNir <jonathan.nir@mail.huji.ac.il>
2024-09-16 17:38:46 +0300

    add TODOs

---
+++ analysis/_default_values/article_figs/_todo_event_matching.py
@@ -0,0 +1,6 @@
+# TODO
+## match ratio
+## matched features
+## match SDT for increasing thresholds (window sizes)
+
+# TODO: add 'show other human' option to figure functions
---
+++ analysis/_default_values/article_figs/_todo_event_timing.py
@@ -0,0 +1,6 @@
+# TODO
+## fixation onsets + offsets
+## saccade onsets + offsets
+## time diffs - fixation & saccades
+
+# TODO: add 'show other human' option to figure functions
