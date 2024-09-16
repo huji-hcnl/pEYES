c4ff45b2992ef0bda0e666a9fef7429b3cd9ec26
JonNir <jonathan.nir@mail.huji.ac.il>
2024-09-16 17:29:28 +0300

    add figures

--- analysis/_default_values/_helpers.py
+++ analysis/_default_values/_helpers.py
@@ -6,6 +6,7 @@ import analysis.utils as u
 DATASET_NAME = "lund2013"
 STIMULUS_TYPE = peyes.constants.IMAGE_STR
 GT1, GT2 = "RA", "MN"
+MULTI_COMP = "fdr_bh"   # FDR Benjamini-Hochberg correction for multiple comparisons
 ALPHA = 0.05
 
 _DEFAULT_VALUES_STR = "default_values"
