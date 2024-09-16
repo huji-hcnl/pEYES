9d3e9ae5913be6bb685d8eeb4c8b36d37753a045
JonNir <jonathan.nir@mail.huji.ac.il>
2024-09-16 11:49:01 +0300

    rename

--- analysis/_default_values/run_processing.py
+++ analysis/_default_values/run_processing.py
@@ -1,12 +1,8 @@
 from analysis._default_values.analysis_config import DATASET_NAME, PROCESSED_DATA_DIR
-from analysis.process.full_pipeline import run
+from analysis.process.full_pipeline import full_pipeline
 
 ###################
 
-results = run(output_dir=PROCESSED_DATA_DIR, dataset_name=DATASET_NAME, verbose=False)
-fix_results = run(
-    output_dir=PROCESSED_DATA_DIR, dataset_name=DATASET_NAME, pos_labels=1, verbose=False
-)
-sac_results = run(
-    output_dir=PROCESSED_DATA_DIR, dataset_name=DATASET_NAME, pos_labels=2, verbose=False
-)
+results = full_pipeline(output_dir=PROCESSED_DATA_DIR, dataset_name=DATASET_NAME, verbose=False)
+fix_results = full_pipeline(output_dir=PROCESSED_DATA_DIR, dataset_name=DATASET_NAME, pos_labels=1, verbose=False)
+sac_results = full_pipeline(output_dir=PROCESSED_DATA_DIR, dataset_name=DATASET_NAME, pos_labels=2, verbose=False)
