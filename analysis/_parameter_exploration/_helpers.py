6a900877e68395b54f184a7983c084db4e82b745
JonNir <jonathan.nir@mail.huji.ac.il>
2024-11-11 14:51:42 +0200

    comparing parameters between articles

---
+++ analysis/_parameter_exploration/_helpers.py
@@ -0,0 +1,67 @@
+import os
+
+import numpy as np
+import peyes
+
+import analysis.utils as u
+
+DATASET_NAME = "lund2013"
+STIMULUS_TYPE = peyes.constants.IMAGE_STR
+GT1, GT2 = "RA", "MN"
+MULTI_COMP = "bonferroni"   # method for multiple comparisons correction: bonferroni, fdr_bh, holm, etc.
+ALPHA = 0.05
+
+_PARAMS_EXPLORATION_STR = "parameter_exploration"
+_FIGURES_STR = "figures"
+PROCESSED_DATA_DIR = os.path.join(u.OUTPUT_DIR, _PARAMS_EXPLORATION_STR)
+FIGURES_DIR = os.path.join(u.OUTPUT_DIR, _PARAMS_EXPLORATION_STR, DATASET_NAME, _FIGURES_STR)
+os.makedirs(FIGURES_DIR, exist_ok=True)
+
+_default_detector_params = dict(missing_value=np.nan, min_event_duration=4, pad_blinks_time=0)
+DETECTORS = {
+    "ivt_andersson": peyes.create_detector(     # Andersson et al. (2017)
+        algorithm="ivt", saccade_velocity_threshold=45, **_default_detector_params
+    ),
+    "ivt_birawo": peyes.create_detector(        # Birawo & Kasprowski (2022)
+        algorithm="ivt", saccade_velocity_threshold=16.5, **_default_detector_params
+    ),
+    # "ivt_salvucci": peyes.create_detector(    # Salvucci & Goldberg (2000)
+    #     algorithm="ivt", saccade_velocity_threshold=100, **_default_detector_params
+    # ),
+    "ivvt": peyes.create_detector(
+        algorithm="ivvt", saccade_velocity_threshold=45, smooth_pursuit_velocity_threshold=26, **_default_detector_params
+    ),
+    "idt_andersson": peyes.create_detector(
+        algorithm="idt", dispersion_threshold=2.7, window_duration=55, **_default_detector_params
+    ),
+    # "idt_birawo": peyes.create_detector(
+    #         # Birawo & Kasprowski (2022) use dispersion of 3.5px and window of 5 samples (10ms)
+    #     algorithm="idt", dispersion_threshold=0.1, window_duration=10, **_default_detector_params
+    # ),
+    "idt_salvucci": peyes.create_detector(
+        algorithm="idt", dispersion_threshold=1, window_duration=100, **_default_detector_params
+    ),
+    "idvt": peyes.create_detector(
+        algorithm="idvt", **_default_detector_params
+    ),
+    "engbert_5": peyes.create_detector(
+        algorithm="engbert", lambda_param=5, **_default_detector_params
+    ),
+    "engbert_6": peyes.create_detector(
+        algorithm="engbert", lambda_param=6, **_default_detector_params
+    ),
+    # "nh": peyes.create_detector(
+    #     algorithm="nh", **_default_detector_params
+    # ),
+    # "remodnav": peyes.create_detector(
+    #     algorithm="remodnav", show_warnings=False, **_default_detector_params
+    # ),
+}
+for key, detector in DETECTORS.items():
+    detector.name = key
+
+FIGURES_CONFIG = {
+    # labeler -> (color, line-style)
+    'Other Human': ("#bab0ac", 'dot'),
+    **{key: (u.DEFAULT_DISCRETE_COLORMAP[i], None) for i, key in enumerate(DETECTORS.keys())}
+}
---
+++ analysis/_parameter_exploration/engbert_lambdas.py
@@ -0,0 +1,124 @@
+#
+# This script is used to explore the effect of the lambda SNR parameter on the Engbert detection algorithm.
+# The following lambdas are compared:
+#   - Engbert & Kliegl (2003) + Andersson et al. (2017): 6.0
+#   - Engbert & Mergenthaler (2006): 5.0
+#
+# We evaluate detection performance compared to human coders on the lund2013+image dataset (only image stimuli).
+# The following metrics are used:
+#   1) overall sample-level agreement - balanced accuracy, Cohen's Kappa, MCC, 1-NLD
+#   2) fixation onset/offset discriminability (d')
+#   3) saccade onset/offset discriminability (d')
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+
+import pandas as pd
+import plotly.io as pio
+
+import peyes
+
+import analysis._parameter_exploration._helpers as h
+
+pio.renderers.default = "browser"
+
+COMPARED_DETECTORS = [det for det in h.DETECTORS.keys() if "engbert" in det.lower()]
+
+# %%
+# SAMPLE-LEVEL AGREEMENT (GLOBAL MEASURES)
+
+import analysis.statistics.sample_metrics as sm
+
+# Load Data
+global_metrics = sm.load_global_metrics(
+    h.DATASET_NAME, h.PROCESSED_DATA_DIR, stimulus_type=h.STIMULUS_TYPE, metric=None, iteration=1,
+)
+global_metrics = pd.concat(
+    [global_metrics.xs(det, axis=1, level='pred', drop_level=False) for det in COMPARED_DETECTORS], axis=1
+).drop(index=peyes.constants.ACCURACY_STR, inplace=False)    # drop Accuracy metric
+
+global_statistics, global_pvalues, global_Ns = sm.mann_whitney(
+    global_metrics, [h.GT1, h.GT2], method='exact'
+)
+
+global_distribution_figure = sm.global_metrics_distributions_figure(global_metrics, h.GT1, h.GT2,)
+global_distribution_figure.show()
+
+print("No statistical difference between λ=5.0 and λ=6.0.")
+
+# %%
+# ONSET & OFFSET DETECTION (FIXATIONS)
+
+import analysis.statistics.channel_sdt as ch_sdt
+
+LABEL = 1       # EventLabelEnum.FIXATION.value
+THRESHOLD = 5   # samples
+
+# Load Data
+fixation_csdt_metrics = ch_sdt.load(
+    dataset_name=h.DATASET_NAME,
+    output_dir=h.PROCESSED_DATA_DIR,
+    label=LABEL,
+    stimulus_type=h.STIMULUS_TYPE,
+    channel_type=None
+)
+fixation_csdt_metrics = pd.concat(
+    [fixation_csdt_metrics.xs(det, axis=1, level='pred', drop_level=False) for det in COMPARED_DETECTORS], axis=1
+)
+
+# calc stats
+fix_onset_u_stat, fix_onset_pvalues, fix_onset_Ns = ch_sdt.mann_whitney(
+    fixation_csdt_metrics, "onset", THRESHOLD, [h.GT1, h.GT2], method='exact',
+)
+
+fix_offset_u_stat, fix_offset_pvalues, fix_offset_Ns = ch_sdt.mann_whitney(
+    fixation_csdt_metrics, "offset", THRESHOLD, [h.GT1, h.GT2], method='exact',
+)
+
+# show figures
+fixation_dprime_figure = ch_sdt.multi_channel_figure(
+    fixation_csdt_metrics,
+    metric=peyes.constants.D_PRIME_STR,
+    yaxis_title=r"$d'$", show_other_gt=True, show_err_bands=True
+)
+fixation_dprime_figure.update_layout(width=1400, height=500,)
+fixation_dprime_figure.show()
+
+print("No statistical difference between λ=5.0 and λ=6.0.")
+
+# %%
+# ONSET & OFFSET DETECTION (SACCADES)
+
+import analysis.statistics.channel_sdt as ch_sdt
+
+LABEL = 2       # EventLabelEnum.SACCADE.value
+THRESHOLD = 5   # samples
+
+# Load Data
+saccade_csdt_metrics = ch_sdt.load(
+    dataset_name=h.DATASET_NAME,
+    output_dir=h.PROCESSED_DATA_DIR,
+    label=LABEL,
+    stimulus_type=h.STIMULUS_TYPE,
+    channel_type=None
+)
+saccade_csdt_metrics = pd.concat(
+    [saccade_csdt_metrics.xs(det, axis=1, level='pred', drop_level=False) for det in COMPARED_DETECTORS], axis=1
+)
+
+# calc stats
+sacc_onset_u_stat, sacc_onset_pvalues, sacc_onset_Ns = ch_sdt.mann_whitney(
+    saccade_csdt_metrics, "onset", THRESHOLD, [h.GT1, h.GT2], method='exact',
+)
+
+sacc_offset_u_stat, sacc_offset_pvalues, sacc_offset_Ns = ch_sdt.mann_whitney(
+    saccade_csdt_metrics, "offset", THRESHOLD, [h.GT1, h.GT2], method='exact',
+)
+
+# show figures
+saccade_dprime_figure = ch_sdt.multi_channel_figure(
+    saccade_csdt_metrics,
+    metric=peyes.constants.D_PRIME_STR,
+    yaxis_title=r"$d'$", show_other_gt=True, show_err_bands=True
+)
+saccade_dprime_figure.update_layout(width=1400, height=500,)
+saccade_dprime_figure.show()
